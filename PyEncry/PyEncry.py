d67c2a0f68021cc5f63ea4b4906fa2afa4d23509
Rick <su.rick@inventec.com>
2018-11-15 10:17:55 +0800

    Rsu: Add version and modify display message.

--- PyEncry/PyEncry.py
+++ PyEncry/PyEncry.py
@@ -3,7 +3,8 @@ import py_compile
 
 os.system("dir/s/b *.py > pyc.txt")
 pyFile = open("pyc.txt", "r")
-print("\n ######## Encryption Start ######## \n")
+print("\n######### PYC tool v0.1 by Rick@20181114 #########")
+print("################ Encryption Start ################ \n")
 for line in pyFile:
 	Offset = line.find('.py')
 	if Offset != -1:
@@ -11,5 +12,5 @@ for line in pyFile:
 		py_compile.compile(line.split("\n")[0])
 pyFile.close()
 os.system("del pyc.txt")
-print("\n ######## Encryption  End  ######## \n")
+print("\n################ Encryption  End  ################ \n")
 print("You can find pyc file in __pycache__ folder")
