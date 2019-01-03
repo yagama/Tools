4f97731bdab5f9d784b926d887b934ce9393edb0
Rick <su.rick@inventec.com>
2019-01-03 13:52:31 +0800

    Excel tool update

--- Excel/BOM.py
+++ Excel/BOM.py
@@ -1,12 +1,14 @@
-from openpyxl import workbook
+from openpyxl import Workbook
 from openpyxl import load_workbook
 
 # 讀檔
 wb = load_workbook('bom.xlsx')
+# 開新檔案
+newwb = Workbook()
 
 # 指定工作表
-sheet = wb.get_sheet_by_name("bom")
-new = wb.get_sheet_by_name("Sheet1")
+sheet = wb.get_sheet_by_name('bom')
+new = newwb.active
 
 #先定義Sheet1要使用的A, B欄row index初始值
 global_A_col = 1	
@@ -26,18 +28,4 @@ for i in sheet["J"]:
 	global_A_col = global_A_col + 1
 		
 #另存新檔，檔名為Final.xlsx
-wb.save('Final.xlsx')
-
-
-# # 獲取A1格的值
-# print(sheet['C9'].value)
-# print(sheet1['A1'].value)
-
-# # 獲取表的最大工作列數
-# print('The max row of this sheet is ' + str(sheet.max_row))
-# # 獲取表的最大工作行數
-# print(sheet.max_column)
-
-# 印出D列的所有內容
-# for i in sheet["D"]:
-    # print(i.value)
+newwb.save('Output.xlsx')
