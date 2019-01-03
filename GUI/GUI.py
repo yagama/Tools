d99167d10009298dce8268d1b9fbd7b2b88b8d1f
Rick <su.rick@inventec.com>
2019-01-03 15:35:46 +0800

    update Entry.set

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -11,24 +11,31 @@ def click():
 	global count
 	count=count + 1
 	step1.configure(text="Yes "+str(count))
+	var=input.get()
+	v3.set(str(count)+"  "+var)
 
+v3 = StringVar()	
+	
 #文字Label
 step1 = tk.Label(text="Step.1 TEXT", width=25)
-input = tk.Entry(root, show="Hello")
+input = tk.Entry(root, show=None)
 count=0
+e3 = tk.Entry(root, textvariable=v3, state='readonly')
 
 #action define
 button1=tk.Button(root, text="Press Me", command=click)
 
 #assign location pack
-# input.pack()
-# step1.pack()
-# button1.pack()
+input.pack()
+step1.pack()
+button1.pack()
+e3.pack()
 
 #assign location grid
 # step1.grid(column=1,row=0)
 # input.grid(column=2,row=1)
 # button1.grid(column=3,row=2)
+# e3.grid(column=4,row=3)
 
 root.mainloop()
 
